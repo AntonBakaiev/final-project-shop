66c6d4dfc0c1df1d9d1e0eab24306bbb0eeb7471
Anton Bakaiev <a777bakaev@gmail.com>
2022-08-10 01:22:18 +0300

    add modules.scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Card from './components/Card';
 import Header from './components/Header';
-import Drawer from './components/Drawer'
+import Drawer from './components/Drawer';
+
+const arr = [
+  { name: 'Обручка1', price: 500, imageURL: '/img/products/001.jpg' },
+  { name: 'Обручка2', price: 544, imageURL: '/img/products/002.jpg' },
+  { name: 'Обручка3', price: 878, imageURL: '/img/products/003.jpg' }
+]
 function App() {
   return (
     <div className="wrapper clear ">
       
       <Drawer />
-      <Header />
+      <Header /> 
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
           <h1>Усі вироби</h1>
@@ -17,7 +23,14 @@ function App() {
         </div>
 
         <div className="d-flex flex-wrap">
-          <Card />
+          {arr.map((obj) => (
+            <Card
+              title={obj.name}
+              price={obj.price}
+              imageURL={ obj.imageURL}
+          />
+          )) }
+          
           
         </div>  
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,17 @@
-function Card() { 
+import styles from './Card.module.scss'
+
+function Card(props) { 
     return (
-        <div className="card">
-            <div className="favorite">
+        <div className={styles.card}>
+            <div className={styles.favorite }>
                 <img src="/img/heartunliked.jpg" alt="heart-unliked" />
             </div>
-            <img width={155} height={112} src="/img/products/001.jpg" alt="" />
-            <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h5>
+            <img width={155} height={112} src={ props.imageURL} alt="" />
+            <h5>{ props.title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Ціна:</span>
-                    <b> 1....1 грн.</b>
+                    <b> { props.price} грн.</b>
                 </div>
                 <button className="button">
                     <img width={11} height={11} src="/img/plus.png" alt="Plus" />
